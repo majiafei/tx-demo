ef3d2918daffde21dae50e25cfd4b18a4116457b
1571221458@qq.com <majiafei>
2019-07-10 22:08:22 +0800

    layui table 解决行高自动调整

---
+++ src/main/java/com/yyt/tx/mjf/entity/UserInfo.java
@@ -0,0 +1,20 @@
+package com.yyt.tx.mjf.entity;
+
+import lombok.Data;
+
+/**
+ * @ProjectName: house
+ * @Package: com.yyt.tx.mjf.entity
+ * @ClassName: UserInfo
+ * @Author: majiafei
+ * @Description:
+ * @Date: 2019/7/10 21:21
+ */
+@Data
+public class UserInfo {
+    private int id;
+
+    private String name;
+
+    private String sex;
+}
