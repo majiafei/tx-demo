72552f7a6cfd9b0056db3fef527afb0ca8f63930
fury36 <40300926+fury36@users.noreply.github.com>
2019-09-06 09:29:08 +0800

    观察者模式

---
+++ src/main/java/com/yyt/tx/mjf/designmodel/observe/Observer.java
@@ -0,0 +1,16 @@
+package com.yyt.tx.mjf.designmodel.observe;
+
+/**
+ * <p>
+ *     观察者接口
+ * </p>
+ * @ClassName: Observer
+ * @Auther: admin
+ * @Date: 2019/9/6 09:10
+ * @Description:
+ */
+public interface Observer {
+
+    void update(String message);
+
+}
